af3465a4d3b7115932b1bae4bb9094c328e7d947
Dayo Greats <dayogreats@gmail.com>
2023-01-16 09:04:53 -0500

    update

--- imagram/imagram/settings.py
+++ imagram/imagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-udky_a!1**vmwdy03b(l1b@axkw$lurb^ozg^xqkv08=+i8wym
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts.apps.PostsConfig',
+    # 'polls.apps.PollsAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- imagram/posts/models.py
+++ imagram/posts/models.py
@@ -6,17 +6,17 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     image = models.ImageField()
-    description = models.TextChoices()
+    description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created =models.DateTimeField(auto_now_add=True)
     created =models.DateTimeField(auto_now=True)
     
 
-    def __str__(self):
-        return 
+    # def __str__(self):
+    #     return self
 
-    def __unicode__(self):
-        return 
+    # def __unicode__(self):
+    #     return 
 
 
 class Comment(models.Model):
@@ -27,10 +27,10 @@ class Comment(models.Model):
     created =models.DateTimeField(auto_now=True)
     
 
-    def __str__(self):
-        return 
+    # def __str__(self):
+    #     return self
 
-    def __unicode__(self):
-        return 
+    # def __unicode__(self):
+    #     return 
 
 
